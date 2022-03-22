4c667538aec455a8f3171cd6411adb99b3419d9a
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-03-22 07:53:08 -0600

    testing

--- datasets/scared_dataset.py
+++ datasets/scared_dataset.py
@@ -39,8 +39,9 @@ class SCAREDRAWDataset(SCAREDDataset):
         super(SCAREDRAWDataset, self).__init__(*args, **kwargs)
 
     def get_image_path(self, folder, frame_index, side):
-        f_str = "{:010d}{}".format(frame_index, self.img_ext)
-        image_path = os.path.join(self.data_path, folder, "image_0{}/data".format(self.side_map[side]), f_str)
+        #f_str = "{:010d}{}".format(frame_index, self.img_ext)
+        #image_path = os.path.join(self.data_path, folder, "image_0{}/data".format(self.side_map[side]), f_str)
+        image_path = os.path.join(self.data_path,folder)
         return image_path
 
     def get_depth(self, folder, frame_index, side, do_flip):

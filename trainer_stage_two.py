f53c3f354f2ecfd2ff375719aa577e9458263fb6
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-03-24 10:56:51 -0600

    testing

--- trainer_stage_two.py
+++ trainer_stage_two.py
@@ -308,8 +308,8 @@ class Trainer:
                     outputs_0 = self.models["position"](position_inputs)
                     outputs_1 = self.models["position"](position_inputs_reverse)
 
-                    print(outputs_0.shape)
-                    print(outputs_1.shape)
+                    print(len(outputs_0))
+                    print(len(outputs_1))
                     for scale in self.opt.scales:
 
                         outputs[("position", scale, f_i)] = outputs_0[("position", scale)]

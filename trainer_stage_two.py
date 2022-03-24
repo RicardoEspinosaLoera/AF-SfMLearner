6a90a6f01695d1c60c164880aa807dc454d3adc6
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-03-24 10:41:50 -0600

    testing

--- trainer_stage_two.py
+++ trainer_stage_two.py
@@ -302,8 +302,8 @@ class Trainer:
                     inputs_all = [pose_feats[f_i], pose_feats[0]]
                     inputs_all_reverse = [pose_feats[0], pose_feats[f_i]]
                     print("predict_poses")
-                    print(inputs_all)
-                    print(inputs_all_reverse)
+                    print(len(inputs_all))
+                    print(len(inputs_all_reverse))
                     # position
                     position_inputs = self.models["position_encoder"](torch.cat(inputs_all, 1))
                     position_inputs_reverse = self.models["position_encoder"](torch.cat(inputs_all_reverse, 1))

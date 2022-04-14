ac9db9a89f9e3906cc444f8fc3e459d030815b6c
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-04-14 08:48:09 -0500

    testing

--- trainer_stage_two.py
+++ trainer_stage_two.py
@@ -308,7 +308,8 @@ class Trainer:
                     outputs_0 = self.models["position"](position_inputs)
                     outputs_1 = self.models["position"](position_inputs_reverse)
 
-                    print(outputs_0['position', 0][0].shape)
+                    print(outputs_0['position', 0][0][0].shape)
+                    print(outputs_0['position', 0][0][1].shape)
                     #print(len(outputs_1))
                     for scale in self.opt.scales:
                         #OG prediction

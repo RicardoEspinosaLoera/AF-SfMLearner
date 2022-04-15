1070910ed16c49389ea3fb1efca7fbfc960e1825
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-04-15 09:18:10 -0500

    testing

--- trainer_stage_two.py
+++ trainer_stage_two.py
@@ -313,7 +313,8 @@ class Trainer:
                     position_inputs_reverse = self.models["position_encoder"](torch.cat(inputs_all_reverse, 1))
                     outputs_0 = self.models["position"](position_inputs)
                     outputs_1 = self.models["position"](position_inputs_reverse)
-                    print(outputs_0['position', 0].shape)
+                    print(outputs_0['position', 0][0][0])
+                    print(outputs_0['position', 0][0][1])
                     #print(outputs_1['position', 0][0][1].shape)
                     #print(len(outputs_1))
                     for scale in self.opt.scales:

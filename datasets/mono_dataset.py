244f6257f833a29d00292e37d4f741ed9bc90ab6
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-05-06 08:44:24 -0500

    testing

--- datasets/mono_dataset.py
+++ datasets/mono_dataset.py
@@ -41,7 +41,7 @@ class MonoDataset(data.Dataset):
                  frame_idxs,
                  num_scales,
                  is_train=False,
-                 img_ext='.jpg'):
+                 img_ext='.png'):
         super(MonoDataset, self).__init__()
 
         self.data_path = data_path

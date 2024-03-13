e93603d0ee548dcc0c3fa026d7cd3b12c0378d7f
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-03-13 12:56:07 -0600

    Hamlyn

--- datasets/scared_dataset.py
+++ datasets/scared_dataset.py
@@ -107,7 +107,11 @@ class SCAREDRAWDataset(SCAREDDataset):
         #COLON10k
         #f_str=str(frame_index) + self.img_ext
         #image_path = os.path.join(self.data_path, folder, f_str)
-            
+        
+        #Hamlyn
+        f_str = "{}{}".format(frame_index, self.img_ext)
+        folder1,folder2 = folder.split("/")
+        image_path = os.path.join(self.data_path, folder1,folder1,folder2, f_str)
         return image_path
 
     def get_depth(self, folder, frame_index, side, do_flip):

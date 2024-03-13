324cecc71eff8fb88f9fe8d77e23c907d0e91041
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-03-13 12:02:16 -0600

    hamlyn

--- datasets/scared_dataset.py
+++ datasets/scared_dataset.py
@@ -73,7 +73,7 @@ class SCAREDDataset(MonoDataset):
         folder = line[0]
 
         if len(line) == 3:
-            frame_index = int(line[1])
+            frame_index = line[1]
         else:
             frame_index = 0
 
@@ -101,8 +101,8 @@ class SCAREDRAWDataset(SCAREDDataset):
 
     def get_image_path(self, folder, frame_index, side):
         #SCATER
-        f_str = "{}{}".format(frame_index, self.img_ext)
-        image_path = os.path.join(self.data_path, folder, "data", f_str)
+        """f_str = "{}{}".format(frame_index, self.img_ext)
+        image_path = os.path.join(self.data_path, folder, "data", f_str)"""
         
         #COLON10k
         #f_str=str(frame_index) + self.img_ext

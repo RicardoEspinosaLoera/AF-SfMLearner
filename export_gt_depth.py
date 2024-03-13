d1d7b3a5f81f4ac768aff198325f71dac5939099
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-03-13 12:37:49 -0600

    generate_depth_map

--- export_gt_depth.py
+++ export_gt_depth.py
@@ -1,14 +1,18 @@
-from __future__ import absolute_import, division, print_function
+# Copyright Niantic 2021. Patent Pending. All rights reserved.
+#
+# This software is licensed under the terms of the ManyDepth licence
+# which allows for non-commercial use only, the full terms of which are made
+# available in the LICENSE file.
+
 
 import os
 
 import argparse
 import numpy as np
 import PIL.Image as pil
-import cv2
 
 from utils import readlines
-from kitti_utils import generate_depth_map
+#from kitti_utils import generate_depth_map
 
 
 def export_gt_depths_kitti():
@@ -23,21 +27,20 @@ def export_gt_depths_kitti():
                         type=str,
                         help='which split to export gt from',
                         required=True,
-                        choices=["eigen", "eigen_benchmark", "endovis"])
+                        choices=["eigen", "eigen_benchmark","hamlyn"])
     opt = parser.parse_args()
 
     split_folder = os.path.join(os.path.dirname(__file__), "splits", opt.split)
     lines = readlines(os.path.join(split_folder, "test_files.txt"))
+
     print("Exporting ground truth depths for {}".format(opt.split))
-    i=0
+
     gt_depths = []
     for line in lines:
-        i = i+1
-        folder, frame_id, _ = line.split()
-        frame_id = int(frame_id)
-        print(i)
-        print(folder)
 
+        #folder, frame_id, _ = line.split()
+        #frame_id = int(frame_id)
+        """
         if opt.split == "eigen":
             calib_dir = os.path.join(opt.data_path, folder.split("/")[0])
             velo_filename = os.path.join(opt.data_path, folder,
@@ -46,23 +49,24 @@ def export_gt_depths_kitti():
         elif opt.split == "eigen_benchmark":
             gt_depth_path = os.path.join(opt.data_path, folder, "proj_depth",
                                          "groundtruth", "image_02", "{:010d}.png".format(frame_id))
-            gt_depth = np.array(pil.open(gt_depth_path)).astype(np.float32) / 256
-        elif opt.split == "endovis":
-            f_str = "scene_points{:06d}.tiff".format(frame_id - 1)
-            gt_depth_path = os.path.join(
-                opt.data_path,
-                folder,
-                "image_02/data/groundtruth",
-                f_str)
-            depth_gt = cv2.imread(gt_depth_path, 3)
-            depth_gt = depth_gt[:, :, 0]
-            gt_depth = depth_gt[0:1024, :]
-
-        gt_depths.append(gt_depth.astype(np.float32))
+            gt_depth = np.array(pil.open(gt_depth_path)).astype(np.float32) / 256"""
+        print(line)
+        folder, file,_ = line.split()
+        folder = folder.split("/")[0]
+        #line = line.replace("/","_")[1:]
+        gt_depth_path = os.path.join(opt.data_path,folder,folder,"depth01", "{}.png".format(file))
+        print(gt_depth_path)
+        #gt_depth = np.array(pil.open(gt_depth_path))
+        im = pil.open(gt_depth_path)
+        newsize = (320, 256)
+        im = im.resize(newsize)
+        gt_depth = np.array(im).astype(np.float16)
+        print(gt_depth.shape)
+        gt_depths.append(gt_depth)
 
     output_path = os.path.join(split_folder, "gt_depths.npz")
 
-    print("Saving to {}".format(opt.split))
+    print("Saving to {} {}".format(opt.split,output_path))
 
     np.savez_compressed(output_path, data=np.array(gt_depths))
 
